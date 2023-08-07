a424929901cdeb09684f093e7da6dc374677b4fc
Cesar David Millan <cesardavidmillan5@gmail.com>
2023-08-07 12:31:31 -0500

    Spring MVC-View Section Completed

--- src/main/java/com/springboot2/springit/SpringitApplication.java
+++ src/main/java/com/springboot2/springit/SpringitApplication.java
@@ -1,14 +1,11 @@
 package com.springboot2.springit;
 
-import com.springboot2.springit.domain.Comment;
-import com.springboot2.springit.domain.Link;
-import com.springboot2.springit.repositories.CommentRepository;
-import com.springboot2.springit.repositories.LinkRepository;
+import org.ocpsoft.prettytime.PrettyTime;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
@@ -20,21 +17,9 @@ public class SpringitApplication {
 		SpringApplication.run(SpringitApplication.class, args);
 	}
 
-//	@Bean
-	CommandLineRunner runner(LinkRepository linkRepository, CommentRepository commentRepository){
-		return args ->{
-			Link link = new Link("Getting Started with Spring Boot 2","https://therealdanvega.com/spring-boot-2");
-			linkRepository.save(link);
-
-			Comment comment = new Comment("This is Spring Boot 2 link is awesome",link);
-			commentRepository.save(comment);
-			link.addComment(comment);
-
-
-			System.out.println("We just inserted a link and a comment");
-			System.out.println("=======================================");
-
-		};
+	@Bean
+	PrettyTime prettyTime(){
+		return new PrettyTime();
 	}
 
 }
--- src/main/java/com/springboot2/springit/domain/Link.java
+++ src/main/java/com/springboot2/springit/domain/Link.java
@@ -1,38 +1,60 @@
 package com.springboot2.springit.domain;
 
-import lombok.Data;
-import lombok.NoArgsConstructor;
-import lombok.NonNull;
-import lombok.RequiredArgsConstructor;
+import com.springboot2.springit.service.BeanUtil;
+import lombok.*;
+import org.ocpsoft.prettytime.PrettyTime;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 import javax.persistence.OneToMany;
+import javax.validation.constraints.NotEmpty;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.time.LocalDateTime;
+import java.time.ZoneId;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 
 @Entity
 @RequiredArgsConstructor
+@Getter @Setter
 @NoArgsConstructor
-@Data
-public class Link extends Auditable{
+public class Link extends Auditable {
 
-    @Id
-    @GeneratedValue
+    @Id @GeneratedValue
     private Long id;
+
     @NonNull
+    @NotEmpty(message = "Please enter a title.")
     private String title;
+
     @NonNull
+    @NotEmpty(message = "Please enter a url.")
     private String url;
 
-    // Comment
     @OneToMany(mappedBy = "link")
     private List<Comment> comments = new ArrayList<>();
 
 
-
-    public void addComment(Comment comment){
+    public void addComment(Comment comment) {
         comments.add(comment);
+    }
+
+
+    public String getDomainName() throws URISyntaxException {
+        URI uri = new URI(this.url);
+        String domain = uri.getHost();
+        return domain.startsWith("www.") ? domain.substring(4) : domain;
+    }
+
+    public String getPrettyTime() {
+        PrettyTime pt = BeanUtil.getBean(PrettyTime.class);
+        return pt.format(convertToDateViaInstant(getCreationDate()));
+    }
+
+    private Date convertToDateViaInstant(LocalDateTime dateToConvert) {
+        return java.util.Date.from(dateToConvert.atZone(ZoneId.systemDefault()).toInstant());
     }
 }
