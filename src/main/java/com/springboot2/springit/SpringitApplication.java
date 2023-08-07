e8d40525cf1ad98063a7427f66e784299a6acd39
Cesar David Millan <cesardavidmillan5@gmail.com>
2023-08-06 22:44:08 -0500

    Bean Commented out

--- src/main/java/com/springboot2/springit/SpringitApplication.java
+++ src/main/java/com/springboot2/springit/SpringitApplication.java
@@ -9,7 +9,6 @@ import org.slf4j.LoggerFactory;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
@@ -21,7 +20,7 @@ public class SpringitApplication {
 		SpringApplication.run(SpringitApplication.class, args);
 	}
 
-	@Bean
+//	@Bean
 	CommandLineRunner runner(LinkRepository linkRepository, CommentRepository commentRepository){
 		return args ->{
 			Link link = new Link("Getting Started with Spring Boot 2","https://therealdanvega.com/spring-boot-2");
